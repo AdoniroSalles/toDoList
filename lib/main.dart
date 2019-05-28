bf11ca83c9acd7f4a6b2c5385ef1eaa2fc93d711
adoniro <adonirosalles@gmail.com>
2019-05-28 02:00:11 -0300

    adicionando metodo para desfazer exclusão

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,8 @@ import 'dart:async';
 import 'dart:io';
 import 'dart:convert';
 
-void main(){
-  runApp( MaterialApp(
+void main() {
+  runApp(MaterialApp(
     home: Home(),
   ));
 }
@@ -16,14 +16,29 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   final _toDoController = TextEditingController();
 
   //lista para armazenar as tarefas
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved; //usado para armazenar cada tarefa removida
+  int _lastRemovedPos; //para saber a posição removida
+
+  //quando inicia o estado
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    //para ler os dados do arquivo, assim que readData obeter os dados
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
 
   //adiciona tarefa
-  void _addToDo(){
+  void _addToDo() {
     //para poder atualizar a lista
     setState(() {
       Map<String, dynamic> newToDo = Map();
@@ -31,6 +46,7 @@ class _HomeState extends State<Home> {
       _toDoController.text = "";
       newToDo["ok"] = false;
       _toDoList.add(newToDo);
+      _saveData();
     });
   }
 
@@ -52,11 +68,10 @@ class _HomeState extends State<Home> {
                   child: TextField(
                     controller: _toDoController,
                     decoration: InputDecoration(
-                      labelText: "Nova Tarefa",
-                      labelStyle: TextStyle(color: Colors.blueAccent)
-                    ),
+                        labelText: "Nova Tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent)),
                   ),
-                ),    
+                ),
                 RaisedButton(
                   color: Colors.blueAccent,
                   child: Text("ADD"),
@@ -67,26 +82,11 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-            //construe a lista conforme for visualizada 
+            //construe a lista conforme for visualizada
             child: ListView.builder(
-              padding  : EdgeInsets.only(top: 10),
+              padding: EdgeInsets.only(top: 10),
               itemCount: _toDoList.length,
-              itemBuilder: (context, index){
-                //index = elemente da lista 
-                return CheckboxListTile(
-                  title: Text(_toDoList[index]["title"]),
-                  value: _toDoList[index]["ok"],
-                  secondary: CircleAvatar( 
-                    child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
-                  ),
-                  onChanged: (event){
-                    //para poder mudar quando for selecionado
-                    setState(() {
-                     _toDoList[index]["ok"] = event; 
-                    });
-                  },
-                );
-              },
+              itemBuilder: buildItem,
             ),
           )
         ],
@@ -94,30 +94,85 @@ class _HomeState extends State<Home> {
     );
   }
 
-  //pega o arquvio json
-  Future<File> _getFile() async{
-    final directory = await getApplicationDocumentsDirectory(); // usada para pegar o diretorio onde possa ser armazenado os documentos do app
-    return File("${directory.path}/data.json");
+  //cria o widget de itens
+  Widget buildItem(BuildContext context, int index) {
+    //index = elemente da lista
+    //Dismissible para poder deslizar o item para o lado e excluir
+    return Dismissible(
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+          alignment: Alignment(-0.9, 0.0),
+          child: Icon(Icons.delete, color: Colors.white),
+        ),
+      ),
+      direction: DismissDirection.startToEnd, //direção para arrastar
+      child: CheckboxListTile(
+        //cria a lista
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+        ),
+        onChanged: (event) {
+          //para poder mudar quando for selecionado
+          setState(() {
+            _toDoList[index]["ok"] = event;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
 
-  } 
+          _saveData();
+
+          //SnackBar: para mostrar alguma informação para o usuario
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
+            ),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+    );
+  }
+
+  //pega o arquvio json
+  Future<File> _getFile() async {
+    final directory =
+        await getApplicationDocumentsDirectory(); // usada para pegar o diretorio onde possa ser armazenado os documentos do app
+    return File("${directory.path}/data.json");
+  }
 
   //salvar dados no  arquivo
-  Future<File> _saveData(_toDoList) async{
-    
-    String data = json.decode(_toDoList);
-    final file  = await _getFile();
+  Future<File> _saveData() async {
+    String data = json.encode(_toDoList);
+    final file = await _getFile();
 
     return file.writeAsString(data);
   }
 
   //ler os dados no arquivo
-  Future<String> _readData() async{
-    try{
+  Future<String> _readData() async {
+    try {
       final file = await _getFile();
-      return file.readAsString(); 
-    }catch(e){
+      return file.readAsString();
+    } catch (e) {
       return null;
     }
   }
 }
-
